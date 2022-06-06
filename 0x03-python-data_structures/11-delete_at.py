9888e3f00ea017ffbb9ee76d2970fdf14061ed3a
briankod <brianmugambi199@gmail.com>
2022-06-06 22:40:28 +0300

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    i = 0
-    while(i < len(my_list)):
-        if idx < 0 or idx >= len(my_list):
-            return(my_list)
-        elif i == idx:
-            del my_list[idx]
-            return(my_list)
-        i += 1
+    if my_list:
+        i = 0
+        while(i < len(my_list)):
+            if idx < 0 or idx >= len(my_list):
+                return(my_list)
+            elif i == idx:
+                del my_list[i]
+                new_list = my_list
+                return(new_list)
+            i += 1
