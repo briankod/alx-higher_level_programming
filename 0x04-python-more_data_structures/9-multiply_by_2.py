7aead03e50d4baf09ca2376d7668c87bba32534c
briankod <brianmugambi199@gmail.com>
2022-06-09 03:56:47 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,7 @@
 def multiply_by_2(a_dictionary):
     if a_dictionary:
         new_dict = a_dictionary.copy()
-        for key in new_dict:
+        key_list = list(new_dict.keys())
+        for key in key_list:
             new_dict[key] *=  2
         return(new_dict)
