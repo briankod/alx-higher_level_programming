c5668b65e47eec0f3fac3bd4982706986ccfdf66
briankod <brianmugambi199@gmail.com>
2022-06-21 21:53:22 +0300

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,23 @@
 #!/usr/bin/bash
+"""A class Square that defines a square"""
+
+
 class Square:
+    """Module of a class
+
+
+    Attributes:
+        __size (int): Describes the length of a square
+    """
     def __init__(self, size):
+        """Initializes the square attributes
+
+
+        Args:
+            size (int): Describes the length of a square
+
+
+        Returns:
+            None
+        """
         self.__size = size
