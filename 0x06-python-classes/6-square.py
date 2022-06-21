ff7a520c2ca897b81dbf7181c6c2813507abb1b8
briankod <brianmugambi199@gmail.com>
2022-06-21 22:59:18 +0300

    Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,7 +23,7 @@ class Square:
             None
         """
         self.__size = size
-        self.__position = position
+        self.position = position
 
     def area(self):
         """Calculates area of a square
