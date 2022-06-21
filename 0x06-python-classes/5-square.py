6f84e38131ba50edb7af8eee6b8c1815e82d392d
briankod <brianmugambi199@gmail.com>
2022-06-21 23:05:32 +0300

    Python - Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -39,6 +39,7 @@ class Square:
         """
         if self.__size == 0:
             print()
+            return
         for i in range(self.__size):
             print("".join(["#" for j in range(self.__size)]))
 
