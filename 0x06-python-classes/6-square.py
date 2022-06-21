794e58b5803c7f686e701ad014613ba948b94c70
briankod <brianmugambi199@gmail.com>
2022-06-21 22:53:38 +0300

    Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,6 +42,7 @@ class Square:
         """
         if self.__size == 0:
             print()
+            return
         for i in range(self.__position[1]):
             print()
         for j in range(self.__size):
