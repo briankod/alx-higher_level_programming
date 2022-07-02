3498bd818e07d9bb3cecb8b58823e823df499a96
briankod <brianmugambi199@gmail.com>
2022-07-02 04:29:10 +0300

    Python - Test-driven development

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -36,5 +36,38 @@ class TestMaxInteger(unittest.TestCase):
         with self.assertRaises(TypeError):
             max_integer(lst)
 
+    def one_element_test(self):
+        """Tests for list of one element"""
+        lst = [2]
+        self.assertEqual(max_integer(lst), 2)
+
+    def one_negative_test(self):
+        """Tests for one negative number in the list"""
+        lst = [36, 72, -36, 4, 3]
+        self.assertEqual(max_integer(lst), 36)
+
+    def all_negative_test(self):
+        """Tests for all negative numbers in the list"""
+        lst = [-2, -31, -10, -11, -22]
+        self.assertEqual(max_integer(lst), -2)
+
+    def max_beginning_test(self):
+        """Tests for max at beginning"""
+        lst = [98, 19, 3, 17]
+        self.assertEqual(max_integer(lst), 98)
+
+    
+    def max_middle_test(self):
+        """Tests for max in middle"""
+        lst = [33, 38, 49, 23, 40]
+        self.assertEqual(max_integer(lst), 49)
+
+    
+    def max_end_test(self):
+        """Tests for max at end"""
+        lst = [59, 52, 25, 67, 90]
+        self.assertEqual(max_integer(lst), 90)
+    
+
 if __name__ == "__main__":
     unittest.main()
