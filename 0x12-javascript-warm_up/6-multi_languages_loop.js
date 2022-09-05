859320204b327729ccafaee73e26478593adf1cc
briankod <brianmugambi199@gmail.com>
2022-09-05 23:58:35 +0300

    JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const arg of args) {
-  console.log(arg);
+for (let i = 0; i < args.length; i++) {
+  console.log(args[i]);
 }
