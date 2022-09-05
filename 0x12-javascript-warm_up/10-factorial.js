9fe9199136678d10fd46504ec72a26cc32f6348f
briankod <brianmugambi199@gmail.com>
2022-09-06 00:26:01 +0300

    JavaScript - Warm up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-'use strict';
-let x = Number(process.argv[2]);
-function factorialize (x) {
+function factorial (x) {
   if (isNaN(x) || x === 1) {
     return (1);
-  } else {
-    return (x * factorialize(x - 1));
   }
+  return x * factorial(x - 1);
 }
-console.log(factorialize(x));
+console.log(factorial(Number(process.argv[2])));
