54278e99440756c46e84209e3f4df34edddb890f
briankod <brianmugambi199@gmail.com>
2022-09-24 02:23:25 +0300

    Python - Network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,13 +2,15 @@
 The first argument will be the repository name.
 The second argument will be the owner name in order.
 You must use the packages requests and sys """
+from urllib import response
 import requests
 from sys import argv
 
 
 if __name__ == '__main__':
-    response = requests.get('https://api.github.com/repos/{}/{}/commits'
-                            .format(argv[2], argv[1]))
+    url = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(argv[2], argv[1]))
+    response = requests.get(url)
     json_obj = response.json()
     for commit in json_obj[:10]:
         print(commit.get('sha'), end=': ')
