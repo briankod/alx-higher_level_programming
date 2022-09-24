45cffa254041c5a59653503a15c98f6796e813d3
briankod <brianmugambi199@gmail.com>
2022-09-24 03:17:32 +0300

    Python - Network #1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,21 +3,20 @@
 The first argument will be the repository name.
 The second argument will be the owner name in order.
 You must use the packages requests and sys """
+from urllib import response
 import requests
 from sys import argv
 
 
 if __name__ == '__main__':
+    url = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(argv[2], argv[1]))
+    response = requests.get(url)
+    json_obj = response.json()
     try:
-        url = requests.get('https://api.github.com/repos/{}/{}/comits'
-                           .format(argv[2], argv[1]))
-        response = requests.get(url)
-        json_obj = response.json()
-        for i, obj in enumerate(json_obj):
-            if i == 10:
-                break
-            if type(obj) is dict:
-                name = obj.get('commit').get('author').get('name')
-                print("{}: {}".format(obj.get('sha'), name))
-    except ValueError as invalid_json:
+        for i in range(10):
+            print("{}: {}".format(
+                json_obj[i].get("sha"),
+                json_obj[i].get("commit").get("author").get("name")))
+    except IndexError:
         pass
