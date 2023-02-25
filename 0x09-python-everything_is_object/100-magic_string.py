8ab08efd79c46ae9a9081d756a310a3acbf399b9
briankod <brianmugambi199@gmail.com>
2023-02-25 22:55:23 +0300

    Python - Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+
+# The magic_string function takes a single argument a, which defaults to
+# an empty list if no value is provided when the function is called.
+# The function then appends the string "BestSchool" to the list a using
+# the += operator, which modifies the original list.
+# Finally, the function returns a string containing all the elements of
+# a, separated by commas and a space, using the join() method with ", "
+# as the separator.
+# It's worth noting that because the default argument value is mutable
+# (a list), if magic_string is called multiple times without explicitly
+# passing in a new list as an argument, the same list object will be
+# used and appended to each time.
 def magic_string(a=[]):
     a += ["BestSchool"]
     return (", ".join(a))
+
+#  A function magic_string() that returns a string “BestSchool” n times
+# the number of the iteration
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,19 @@
 """A class LockedClass with no class or object attribute"""
 
 
+# The __slots__ attribute is used to define a fixed set of allowed instance
+# attributes for the class. In this case, the only allowed instance attribute
+# is first_name. Attempting to assign any other attribute to an instance of
+# this class will raise an AttributeError.
+# By using __slots__, you can save memory and improve performance for instances
+# of this class, because Python does not need to create a dictionary to store
+# instance attributes. Instead, it stores the attribute values in a tuple-like
+# structure that is more memory-efficient.
 class LockedClass:
     """The module prevents the user from dynamically creating new instance
     attributes,except if the new instance attribute is called first_name"""
     __slots__ = ["first_name"]
+
+# A class LockedClass with no class or object attribute, that prevents the
+# user from dynamically creating new instance attributes, except if the new
+# instance attribute is called first_name.
